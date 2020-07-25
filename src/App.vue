b80cdca71e4ac7f30a8121d229834378702a5b6d
Sethu Palanisamy <rpsethu1471@gmail.com>
2020-07-25 16:36:35 +0530

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app >
+    <v-main>
+      <v-row>
+        <v-col cols="12" sm="11" md="11" class="layout align-center justify-center">
+          <HelloWorld />
+        </v-col>
+        <v-col cols="12" sm="11" md="1" >
+         <v-switch v-model="switch1" ></v-switch>
+        </v-col>
+      </v-row>
+     
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
+
   components: {
     HelloWorld
+  },
+
+  data: () => ({
+    switch1: false,
+    //
+  }),
+  updated(){
+    console.log(this.switch1);
+    this.$vuetify.theme.dark = this.switch1;
   }
-}
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+.redColor{
+  color: #E65100;
+}
+.greenColor{
+  color: #4CAF50;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,172 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container >
+     <v-snackbar
+      v-model="snackbar"
+      top
+      color="indigo"
+      class="white--text"
+    >
+    {{ snackbarText }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <v-row>
+      <v-col cols="12" sm="6" md="3">
+        <v-select
+        v-model="model"
+        :items="type"
+        :append-icon="appendIcon ? 'mdi-plus' : ''"
+        label="Type"
+        outlined
+      >
+         </v-select>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+          <v-text-field
+          v-model="reason"
+            label="Transcation Reason"
+            outlined
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field
+            v-model="amount"
+            label="Amount"
+            outlined
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          <v-btn dark class="indigo white--text" @click="process">Procced</v-btn>
+      </v-col>
+    </v-row>
+    <v-card>
+    <v-card-title>
+      Balance: {{balance}}
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="5"
+    :item-class= "row_classes"
+    class="elevation-1"
+  ></v-data-table>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      snackbar: null,
+      snackbarText: "SomeError",
+      balance: 0,
+      appendIcon: true,
+       type: ['Credit', 'Debit'],
+       model: null,
+       reason: null,
+       amount: null,
+       search: null,
+       headers:[ {
+            text: 'S.no',
+            align: 'start',
+            sortable: false,
+            value: 'sno',
+          },
+          { text: 'Type',align: 'start', value: 'type' },
+          { text: 'Reason',sortable: false,align: 'start', value: 'reason',color: "red" },
+          { text: 'Balance',align: 'start', value: 'balance' },
+          { text: 'Credit/Debit Amount',align: 'start', value: 'typeCredit' },
+          { text: 'Date',align: 'start', value: 'date' },
+          ],
+          items:[]
+    }),
+  methods:{
+    row_classes(item) {
+        if (item.type === "Debit") {
+          return 'redColor'
+        } else if(item.type === "Credit"){
+          return 'greenColor'
+        }
+    },
+    add(x, y){
+      var a = parseFloat(x);
+      var b = parseFloat(y);
+      return (a+b);
+    },
+    minus(x,y){
+      var a = parseFloat(x);
+      var b = parseFloat(y);
+      return (a-b);
+    },
+    process(){
+      console.log(this.$vuetify.theme.dark);
+      var m = parseFloat(this.amount)
+      var n = m.toString()
+      if(n === this.amount){
+        if(this.reason !== null){
+        if(this.model === "Credit"){
+        //
+        var amt = this.add(this.balance, this.amount);
+        this.balance = amt;
+        this.items.push({
+          sno: this.items.length + 1,
+          type: this.model,
+          reason: this.reason,
+          balance: amt,
+          typeCredit: this.amount,
+          date: new Date()
+        })
+
+      }else if(this.model === "Debit"){
+        //
+        var amtMinus = this.minus(this.balance, this.amount);
+        this.balance = amtMinus;
+        this.items.push({
+          sno: this.items.length + 1,
+          type: this.model,
+          reason: this.reason,
+          balance: amtMinus,
+          typeCredit: this.amount,
+          date: new Date()
+        })
+      }
+      else{
+        this.snackbar = true;
+        this.snackbarText = "Select Credit or Debit"
+      }}else{
+        this.snackbar = true;
+        this.snackbarText = "Enter Reason"
+      }
+      }else{
+        this.snackbar = true;
+        this.snackbarText = "Invalid Amount"
+      }
+    }
   }
-}
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped>
+.green{
+  background-color: green;
+  font-size: 60px;
 }
 </style>
